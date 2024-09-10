603fcbe80f282dc17c9c95353f3e730d280a8277
ila36IX <jbariali002@gmail.com>
2024-09-10 01:41:29 +0100

    Unittests and Integration Tests

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python3
+"""
+Integration tests aim to test a code path end-to-end. In general, only low
+level functions that make external calls such as HTTP requests, file I/O,
+database I/O, etc. are mocked.
+"""
+
+import unittest
+from parameterized import parameterized
+from utils import access_nested_map, get_json, memoize
+from unittest.mock import patch, Mock
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """
+    documentation is important thing
+    """
+
+    @parameterized.expand(
+        [
+            ({"a": 1}, ("a",), 1),
+            ({"a": {"b": 2}}, ("a",), {"b": 2}),
+            ({"a": {"b": 2}}, ("a", "b"), 2)
+        ]
+    )
+    def test_access_nested_map(self, nested_map, path, expected_output):
+        """
+        documentation is important thing
+        """
+        result = access_nested_map(nested_map, path)
+        self.assertEqual(result, expected_output)
+
+    @parameterized.expand(
+        [
+            ({}, ("a",), KeyError),
+            ({"a": 1}, ("a", "b"), KeyError)
+        ]
+    )
+    def test_access_nested_map_exception(self, nested_map, path,
+                                         expected_output):
+        """
+        documentation is important thing
+        """
+        with self.assertRaises(expected_output) as context:
+            access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """
+    documentation is important thing
+    """
+    @parameterized.expand(
+        [
+            ('http://example.com', {'payload': True}),
+            ('http://holberton.io', {'payload': False})
+        ]
+    )
+    def test_get_json(self, url, expected_output):
+        """_summary_
+        """
+        mock_response = Mock()
+        mock_response.json.return_value = expected_output
+        with patch('requests.get', return_value=mock_response):
+            response = get_json(url)
+
+            self.assertEqual(response, expected_output)
+
+
+class TestMemoize(unittest.TestCase):
+    """
+    documentation is important thing
+    """
+    def test_memoize(self):
+        """
+        documentation is important thing
+        """
+
+        class TestClass:
+            """
+            documentation is important thing
+            """
+
+            def a_method(self):
+                """
+                documentation is important thing
+                """
+                return 42
+
+            @memoize
+            def a_property(self):
+                """
+                documentation is important thing
+                """
+                return self.a_method()
+
+        test_obj = TestClass()
+
+        with patch.object(test_obj, 'a_method') as mock_method:
+            mock_method.return_value = 42
+
+            result1 = test_obj.a_property
+            result2 = test_obj.a_property
+
+            self.assertEqual(result1, 42)
+            self.assertEqual(result2, 42)
+            mock_method.assert_called_once()
